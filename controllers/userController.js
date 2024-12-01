f3aa173c933b134f988c43271086855ebac69040
Deshan Chinthana <chamildes@gmail.com>
2024-12-01 23:08:49 +0530

    loginUser converted to an async function

--- controllers/userController.js
+++ controllers/userController.js
@@ -39,58 +39,55 @@ export async function createUser(req, res) {
   }
 }
 
-// is the promise created using .then
-// export function createUser(req,res){
+export async function loginUser(req, res) {
+  try {
+    // Find the user by email
+    const users = await User.find({ email: req.body.email });
 
-//   const newUserData = req.body
-//   newUserData.password = bcrypt.hashSync(newUserData.password, 10)  
-//   const user = new User(newUserData)
-
-//   user.save().then(()=>{
-//     res.json({
-//       message: "User created"
-//     })
-//   }).catch((error)=>{
-//     res.json({      
-//       message: "User not created"
-//     })
-//   })
-// }
-
-export function loginUser(req, res) {
-
-  User.find({ email: req.body.email }).then(
-    (users) => {
-      if (users.length == 0) {
-        res.json({
-          message: "User not found"
-        })
-      } else {
-        const user = users[0]
-        const isPasswordCorrect = bcrypt.compareSync(req.body.password, user.password)
-
-        if (isPasswordCorrect) {
-
-          const token = jwt.sign({
-            email: user.email,
-            firstName: user.firstName,
-            lastName: user.lastName,
-            isBlocked: user.isBlocked,
-            type: user.type,
-            profilePicture: user.profilePicture
-          }, process.env.JWT_SECRET)
-
-          res.json({
-            message: "User logged in",
-            token: token
-          })
-
-        } else {
-          res.json({
-            message: "User not logged in (wrong password)"
-          })
-        }
-      }
+    // If no users are found, send a "User not found" response
+    if (users.length === 0) {
+      return res.json({
+        message: "User not found"
+      });
     }
-  )
+
+    // Get the first user, email is unique
+    const user = users[0];
+
+    // Compare the provided password with the hashed password
+    const isPasswordCorrect = await bcrypt.compare(req.body.password, user.password);
+
+    if (isPasswordCorrect) {
+      // Generate a JWT token
+      const token = jwt.sign(
+        {
+          email: user.email,
+          firstName: user.firstName,
+          lastName: user.lastName,
+          isBlocked: user.isBlocked,
+          type: user.type,
+          profilePicture: user.profilePicture
+        },
+        process.env.JWT_SECRET,
+        // { expiresIn: '1h' } // Optional: token expiration
+      );
+
+      // Send a success response with the token
+      res.json({
+        message: "User logged in",
+        token: token
+      });
+    } else {
+      // Send a "wrong password" response
+      res.json({
+        message: "User not logged in (wrong password)"
+      });
+    }
+  } catch (error) {
+    // Handle any unexpected errors
+    res.status(500).json({
+      message: "An error occurred during login",
+      error: error.message
+    });
+  }
 }
