2f69e41d386a0b2265085f6863fdf2f843d89ccf
Deshan Chinthana <chamildes@gmail.com>
2024-12-03 22:52:43 +0530

    orderRouter added to the index.js and fixed the error with adding file type .js in imports of orderRouter

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
 import userRouter from './routes/userRouter.js';
 import productRouter from './routes/productRouter.js';
+import orderRouter from './routes/orderRouter.js';
 
 dotenv.config()
 
@@ -38,6 +39,7 @@ app.use(
 
 app.use("/api/users", userRouter)
 app.use("/api/products", productRouter)
+app.use("/api/orders", orderRouter)
 
 app.listen(
   5000,
--- routes/orderRouter.js
+++ routes/orderRouter.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { createOrder, getOrders } from '../controllers/orderController';
+import { createOrder, getOrders } from '../controllers/orderController.js';
 
 const orderRouter = express.Router();
 
