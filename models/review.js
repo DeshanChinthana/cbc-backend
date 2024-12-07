bc28a6477b90b01c81f811f69cfa36f6c13ea13f
Deshan Chinthana <chamildes@gmail.com>
2024-12-07 13:26:03 +0530

    review model created with references to the users and products

---
+++ models/review.js
@@ -0,0 +1,37 @@
+import mongoose from "mongoose";
+
+const reviewSchema = mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "users", // Refers to the 'users' collection
+      required: true
+    },
+    productId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "products", // Refers to the 'products' collection
+      required: true
+    },
+    rating: {
+      type: Number,
+      required: true,
+      min: 1,
+      max: 5
+    },
+    comment: {
+      type: String,
+      default: ""
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now
+    }
+  },
+  {
+    timestamps: true // Automatically adds createdAt and updatedAt fields
+  }
+);
+
+const Review = mongoose.model("reviews", reviewSchema);
+
+export default Review;
